8892fd003aee71b43f0c539d83f8246de5135f82
Lucas <lucasanmelo@gmail.com>
2019-07-29 13:09:42 -0700

    Remove incomplete macs2 files, clarified submit_pipeline.sh

--- example_project/scripts/submit_pipeline.sh
+++ example_project/scripts/submit_pipeline.sh
@@ -15,14 +15,16 @@ export logs_dir=$project_dir/logs
 # see https://aws.amazon.com/ec2/instance-types/
 export vCPU=1
 
+# define location of pipelines/steps directory
+export steps=/shared/pipelines/steps
+
 # define required variables
 export star_index=/shared/lib/indexes/star-grch38-97/
 export rsem_index=/shared/lib/indexes/rsem-grch38-97/rsem-grch38-97
 
 # run pipeline
-pipeline=/path/to/pipeline.sh
+pipeline=/shared/pipelines/RNA-seq/trimgalore_star_rsem.sh
 
 while read line; do
-	$pipeline $line
+        $pipeline $line
 done < reads.txt
-
